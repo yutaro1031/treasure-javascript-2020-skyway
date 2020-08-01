a2ab56d27d20e7f587016318f8e98adaf9c3f6f0
yutaro1031 <40020812+yutaro1031@users.noreply.github.com>
2020-08-01 16:47:12 +0900

    通話できるように

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 (async function(){
+    let localStream;
     try {
-        const localStream = await navigator.mediaDevices.getUserMedia({video: true, audio: true});
+        localStream = await navigator.mediaDevices.getUserMedia({video: true, audio: true});
         const videoElement = document.getElementById('my-video');
         videoElement.srcObject = localStream;
         videoElement.play();
@@ -15,5 +16,28 @@
     //PeerID取得
     peer.on('open', () => {
         document.getElementById('my-id').textContent = peer.id;
+    });
+
+    // イベントリスナを設置する関数
+    const setEventListener = mediaConnection => {
+        mediaConnection.on('stream', stream => {
+            // video要素にカメラ映像をセットして再生
+            const videoElm = document.getElementById('their-video')
+            videoElm.srcObject = stream;
+            videoElm.play();
+        });
+    }
+
+    // 発信処理
+    document.getElementById('make-call').onclick = () => {
+        const theirID = document.getElementById('their-id').value;
+        const mediaConnection = peer.call(theirID, localStream);
+        setEventListener(mediaConnection);
+    };
+
+    //着信処理
+    peer.on('call', mediaConnection => {
+        mediaConnection.answer(localStream);
+        setEventListener(mediaConnection);
     });
 })();
