4252be077deac3e1b80ed182d3dc32031fad27c0
yutaro1031 <40020812+yutaro1031@users.noreply.github.com>
2020-08-01 16:33:27 +0900

    peerIdであれこれする

--- index.js
+++ index.js
@@ -7,4 +7,13 @@
     }catch(error){
         alert(error);
     }
+
+    const peer = new Peer({
+        key: 'd9547008-e4bd-4d2e-b44d-bfa733153a8b',
+        debug: 3
+    });
+    //PeerID取得
+    peer.on('open', () => {
+        document.getElementById('my-id').textContent = peer.id;
+    });
 })();
