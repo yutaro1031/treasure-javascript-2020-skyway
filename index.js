ac8ab003c7fe79223e75e50a79d805482d68a704
yutaro1031 <40020812+yutaro1031@users.noreply.github.com>
2020-08-01 16:15:29 +0900

    video映るように

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
-(function(){
-    console.log("hello treasure");
+(async function(){
+    try {
+        const localStream = await navigator.mediaDevices.getUserMedia({video: true, audio: true});
+        const videoElement = document.getElementById('my-video');
+        videoElement.srcObject = localStream;
+        videoElement.play();
+    }catch(error){
+        alert(error);
+    }
 })();
